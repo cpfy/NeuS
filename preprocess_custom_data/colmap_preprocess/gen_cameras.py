ee0b6a10f813f1ce0709dfc7fd2ef5b75a46429d
cpfy <309301477@qq.com>
2023-02-02 11:55:14 +0800

    fix: 解决相机参数npz无法正确生成的问题
    参照LLFF代码的一个未合并PR #61，以及NeuS issues#50中的补充（https://github.com/Totoro97/NeuS/issues/50#issuecomment-1154528508）

--- preprocess_custom_data/colmap_preprocess/gen_cameras.py
+++ preprocess_custom_data/colmap_preprocess/gen_cameras.py
@@ -68,13 +68,30 @@ if __name__ == '__main__':
     os.makedirs(os.path.join(out_dir, 'image'), exist_ok=True)
     os.makedirs(os.path.join(out_dir, 'mask'), exist_ok=True)
 
-    image_list = glob(os.path.join(work_dir, 'images/*.png'))
+    # image_list = glob(os.path.join(work_dir, 'images/*.png'))
+    # image_list.sort()
+
+    # for i, image_path in enumerate(image_list):
+    #     img = cv.imread(image_path)
+    #     cv.imwrite(os.path.join(out_dir, 'image', '{:0>3d}.png'.format(i)), img)
+    #     cv.imwrite(os.path.join(out_dir, 'mask', '{:0>3d}.png'.format(i)), np.ones_like(img) * 255)
+
+    # np.savez(os.path.join(out_dir, 'cameras_sphere.npz'), **cam_dict)
+    # print('Process done!')
+
+    #New!
+    with open(os.path.join(work_dir,"view_imgs.txt"), "r") as f:
+        image_list = f.read().split('\n')
+    
+    # image_list = glob(os.path.join(work_dir, 'images/*.png'))
     image_list.sort()
 
     for i, image_path in enumerate(image_list):
-        img = cv.imread(image_path)
+        img = cv.imread(os.path.join(work_dir,'image',image_path))
         cv.imwrite(os.path.join(out_dir, 'image', '{:0>3d}.png'.format(i)), img)
         cv.imwrite(os.path.join(out_dir, 'mask', '{:0>3d}.png'.format(i)), np.ones_like(img) * 255)
-
+    
     np.savez(os.path.join(out_dir, 'cameras_sphere.npz'), **cam_dict)
-    print('Process done!')
+    #New!
+
+    print('New Process done!')
